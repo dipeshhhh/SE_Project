588562c6a56f4c62e0462634bb930b7322e626ea
Dipesh <dipesh.productivity@gmail.com>
2023-04-27 19:42:37 +0530

    BugFix: lname column in users was set to INT, set it to VARCHAR(100)

--- res/database/monkeylearn.sql
+++ res/database/monkeylearn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2023 at 04:09 PM
+-- Generation Time: Apr 27, 2023 at 04:11 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -107,7 +107,7 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
   `fname` varchar(100) NOT NULL,
-  `lname` int(100) DEFAULT NULL,
+  `lname` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
